73163f4415c5a8fef18811351b8eb301cd7be2f3
Vinícius Naves <vinicius_epiplon@hotmail.com>
2017-04-10 16:33:01 -0300

    Timed door (not working yet)

--- Project Mazerunner/Assets/Scripts/DoorController.cs
+++ Project Mazerunner/Assets/Scripts/DoorController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.Networking;
 
 [RequireComponent(typeof(Animator))]
@@ -8,6 +9,7 @@ public class DoorController : NetworkBehaviour
 {
     NetworkAnimator animator;
     [SerializeField] public string OpenParameter;
+    [SerializeField] public string CloseParameter;
 
     [Header("Configurations")]
     [SerializeField] public DoorType doorType;
@@ -17,8 +19,29 @@ public class DoorController : NetworkBehaviour
     [SyncVar] int _count = 0;
     #endregion
 
-    void Start () {
+    #region Timed
+    [SerializeField] public float Timer;
+    [SyncVar] float _timer = 0;
+    [SyncVar] bool isOpen = false;
+    [SerializeField] public UnityEvent OnClose;
+    #endregion
+
+    void Start()
+    {
         animator = GetComponent<NetworkAnimator>();
+    }
+
+    private void Update()
+    {
+        if (isServer && _timer > 0 && !isOpen)
+        {
+            _timer -= Time.deltaTime;
+            if (_timer <= 0) // Close doors
+            {
+                _count = 0;
+                Close();
+            }
+        }
     }
 
     public void Activate()
@@ -34,14 +57,41 @@ public class DoorController : NetworkBehaviour
                     Open();
 
                 break;
+            case DoorType.Timed:
+                if (!isOpen) // If door is closed
+                {
+                    if (_count == 0) // start timer
+                    {
+                        _timer = Timer;
+                        _count++;
+                    }
+                    else if (_timer > 0)
+                    {
+                        _count++;
+                    }
+
+                    if (_count >= TotalCount) // if count is 
+                    {
+                        Open();
+                    }
+                }
+                break;
             default:
                 break;
         }
     }
-	
+
     void Open()
     {
         animator.SetTrigger(OpenParameter);
+        isOpen = true;
+    }
+
+    void Close()
+    {
+        animator.SetTrigger(CloseParameter);
+        isOpen = false;
+        OnClose.Invoke();
     }
 }
 
@@ -49,5 +99,5 @@ public enum DoorType
 {
     Simple,
     Numbered,
-    //Timed
+    Timed
 }
--- Project Mazerunner/Assets/Scripts/Editor/DoorControllerEditor.cs
+++ Project Mazerunner/Assets/Scripts/Editor/DoorControllerEditor.cs
@@ -5,21 +5,31 @@ using UnityEngine;
 [CustomEditor(typeof(DoorController))]
 public class DoorControllerEditor : Editor
 {
+    SerializedProperty openParameter;
+    SerializedProperty closeParameter;
+    
     SerializedProperty doorType;
     SerializedProperty totalCount;
-    SerializedProperty openParameter;
+    SerializedProperty timer;
+    SerializedProperty onclose;
 
     private void OnEnable()
     {
         openParameter = serializedObject.FindProperty("OpenParameter");
-        totalCount = serializedObject.FindProperty("TotalCount");
+        closeParameter = serializedObject.FindProperty("CloseParameter");
+
+
         doorType = serializedObject.FindProperty("doorType");
+        totalCount = serializedObject.FindProperty("TotalCount");
+        timer = serializedObject.FindProperty("Timer");
+        onclose = serializedObject.FindProperty("OnClose");
     }
 
     public override void OnInspectorGUI()
     {
         EditorGUILayout.BeginVertical(EditorStyles.inspectorDefaultMargins);
-        openParameter.stringValue = EditorGUILayout.TextField("Trigger Parameter", openParameter.stringValue);
+        openParameter.stringValue = EditorGUILayout.TextField("Open Parameter", openParameter.stringValue);
+        closeParameter.stringValue = EditorGUILayout.TextField("Close Parameter", closeParameter.stringValue);
         EditorGUILayout.Space();
 
         EditorGUILayout.LabelField("Configurations", EditorStyles.boldLabel);
@@ -30,8 +40,15 @@ public class DoorControllerEditor : Editor
                 EditorGUILayout.HelpBox("Door will open as soon as the button is activated.", MessageType.Info);
                 break;
             case DoorType.Numbered:
-                totalCount.intValue = EditorGUILayout.IntField("Total Count", totalCount.intValue);
+                totalCount.intValue = EditorGUILayout.IntField("Buttons", totalCount.intValue);
                 EditorGUILayout.HelpBox("Number of buttons that must be activated so the door can open.", MessageType.Info);
+                break;
+            case DoorType.Timed:
+                timer.floatValue = EditorGUILayout.FloatField("Timing", timer.floatValue);
+                totalCount.intValue = EditorGUILayout.IntField("Buttons", totalCount.intValue);
+                EditorGUILayout.HelpBox("A number of buttons need to be activated before time runs out.", MessageType.Info);
+
+                EditorGUILayout.PropertyField(onclose);
                 break;
             default:
                 break;
